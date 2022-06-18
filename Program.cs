93b19ab7d699ecb183ad5119b747bd648dcc8f7d
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-06-18 16:04:52 +0200

    added dictionary and datetime

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@
 		// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
 		public bool Bool { get; } = true;
 		public byte Byte { get; } = 123;
-		public sbyte SByte { get; } = 456;
-		public char Char { { get; } = 'a';
+		public sbyte SByte { get; } = 111;
+		public char Char { get; } = 'a';
 		public decimal Decimal { get; } = .3;
 		public double Double { get; } = 0.0003;
 		public float Float { get; } = 0.0023f;
@@ -20,11 +20,17 @@
 		public short Short { get; } = 8;
 		public ushort UShort { get; } = 9;
 
+		public object Object { get; } = new Program();
+		public string String { get; } = "test string";
+		public dynamic Dynamic { get; } = new Program();
 
+		public DateTime DateTime { get; } = DateTime.UtcNow;
+		public Dictionary<string, int> Dictionary { get; } = new Dictionary<string,int>() {{"one", 1}, {"two", 2}, {"two", }}
 		public void HelloWorld()
 		{
 			writeLn("Hello World from Node.js");
 			WebAssembly.Eval("console.log(\"Hello via eval\")");
+
 		}
 	}
 }
