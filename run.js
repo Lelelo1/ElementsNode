f2711c0cae9dab666fe61b20ededbaf372d21dbb
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-06-18 16:40:34 +0200

    added calls to elements properties

--- run.js
+++ run.js
@@ -10,4 +10,27 @@ CompileTest.instantiate((source)).then(function (result) {
 			console.log("WebAssembly file CompileTest.wasm has been loaded.");
 			var program = result.Program();
 			program.HelloWorld();
+            
+            console.log(program.Bool);
+            console.log(program.Byte);
+            console.log(program.SByte);
+            console.log(program.Char);
+            console.log(program.Decimal);
+            console.log(program.Double);
+            console.log(program.Float);
+            console.log(program.Int);
+            console.log(program.UInt);
+            console.log(program.Nint);
+            console.log(program.NUInt);
+            console.log(program.Long);
+            console.log(program.ULong);
+            console.log(program.Short);
+            console.log(program.UShort);
+
+            console.log(program.Object);
+            console.log(program.String);
+            console.log(program.Dynamic);
+
+            console.log(program.DateTime);
+            console.log(program.Dictionary);
 		});
